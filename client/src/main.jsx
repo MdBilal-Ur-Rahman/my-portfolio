e2ccb9c6433bfe4a1c6343eb06cf89cb6a6cc089
Md-Bilal762 <mdbilalurrahman@outlook.com>
2026-07-05 00:24:03 +0530

    Add Google Analytics

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import ReactGA from "react-ga4";
+
 import "./index.css";
 import App from "./App.jsx";
+
+// Initialize Google Analytics
+ReactGA.initialize("G-2ZMMWQ1LEE");
+
+// Track page view
+ReactGA.send({
+  hitType: "pageview",
+  page: window.location.pathname + window.location.search,
+});
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
