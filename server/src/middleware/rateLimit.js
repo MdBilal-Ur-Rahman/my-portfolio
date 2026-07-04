59a14e19a2fc0c303c6d7591cc52dacea484fa06
Md-Bilal762 <mdbilalurrahman@outlook.com>
2026-07-04 14:40:14 +0530

    Increase rate limit

--- server/src/middleware/rateLimit.js
+++ server/src/middleware/rateLimit.js
@@ -3,7 +3,7 @@ import rateLimit from "express-rate-limit";
 // Rate Limiter configuration
 const rateLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 3, // max 100 requests per IP
+  max: 100, // max 100 requests per IP
   message: {
     success: false,
     message: "Too many requests. Please try again after 15 minutes.",
