2fa00f27acfad8db77fea2be796784c0afc40049
Md-Bilal762 <mdbilalurrahman@outlook.com>
2026-07-04 18:48:29 +0530

    Use Gmail service for Nodemailer

--- server/src/services/emailService.js
+++ server/src/services/emailService.js
@@ -3,16 +3,29 @@ import nodemailer from "nodemailer";
 
 dotenv.config();
 
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
 
+
+
+
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
 });
+
+// const transporter = nodemailer.createTransport({
+//   host: "smtp.gmail.com",
+//   port: 465,
+//   secure: true,
+
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS,
+//   },
+// });
 
 export const sendEmail = async ({
   name,
